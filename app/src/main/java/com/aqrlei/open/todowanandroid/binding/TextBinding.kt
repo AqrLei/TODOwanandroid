bd0e91d977c5ded392832c9548eeb7b83f0b0f4a
AqrLei <leilei@guoshujinfu.com>
2018-12-28 18:45:51 +0800

    Improve

---
+++ app/src/main/java/com/aqrlei/open/todowanandroid/binding/TextBinding.kt
@@ -0,0 +1,11 @@
+package com.aqrlei.open.todowanandroid.binding
+
+import com.google.android.material.textfield.TextInputLayout
+
+/**
+ * @author aqrlei on 2018/12/28
+ */
+
+fun TextInputLayout.setErrorMsg(text: String) {
+    this.error = text
+}
