14d0d30a84aa843f3011f340c52ae66bb0749ba8
AqrLei <leilei@guoshujinfu.com>
2019-01-02 17:46:29 +0800

    Improve

--- app/src/main/java/com/aqrlei/open/todowanandroid/base/ViewModelFragment.kt
+++ app/src/main/java/com/aqrlei/open/todowanandroid/base/ViewModelFragment.kt
@@ -4,30 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
 /**
  * @author aqrlei on 2018/12/24
  */
-abstract class ViewModelFragment<VM : BaseViewModel> : Fragment(), BaseView {
+abstract class ViewModelFragment<VM : BaseViewModel, VB : ViewDataBinding> : Fragment(), BaseView {
 
     protected abstract val viewModel: VM
+    private lateinit var binding: VB
 
     private val baseView: BaseView?
         get() = this.context as? BaseView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(bindLayout(), container, false)
+        binding = DataBindingUtil.inflate(inflater, bindLayout(), container, false)
+        return binding.root
+
     }
 
     abstract fun bindLayout(): Int
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        observerData()
+        initComponents(binding)
     }
 
-    abstract fun observerData()
+    abstract fun initComponents(binding: VB)
 
     override fun showToast(msg: String) {
         baseView?.showToast(msg)
