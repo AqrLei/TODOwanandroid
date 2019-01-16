b0030fc4d50e493c410ce1a08548d052a2a37fba
AqrLei <leilei@guoshujinfu.com>
2019-01-16 09:14:43 +0800

    Improve

--- bindingadapter/src/main/kotlin/com/aqrlei/open/bindingadapter/bind/OnItemBind.kt
+++
@@ -1,8 +0,0 @@
-package com.aqrlei.open.bindingadapter.bind
-
-/**
- * @author aqrlei on 2019/1/11
- */
-interface OnItemBind<T> {
-    fun onItemBind(itemBinding: ItemBinding<T>, position: Int, item: T)
-}
