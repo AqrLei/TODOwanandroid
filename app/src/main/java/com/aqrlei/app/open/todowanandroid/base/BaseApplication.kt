52f3bfcdf7bf6df85b2cf03201de7f5010db00d3
AqrLei <leilei@guoshujinfu.com>
2019-03-11 10:37:54 +0800

    update Gradle & add Strict Mode

--- app/src/main/java/com/aqrlei/app/open/todowanandroid/base/BaseApplication.kt
+++ app/src/main/java/com/aqrlei/app/open/todowanandroid/base/BaseApplication.kt
@@ -3,7 +3,9 @@ package com.aqrlei.app.open.todowanandroid.base
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import android.os.StrictMode
 import android.util.Log
+import com.aqrlei.app.open.todowanandroid.BuildConfig
 import com.aqrlei.open.utils.AppCache
 import com.aqrlei.open.utils.ToastHelper
 
@@ -21,34 +23,52 @@ class BaseApplication : Application.ActivityLifecycleCallbacks, Application() {
     override fun onCreate() {
         super.onCreate()
         ToastHelper.init(this)
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build())
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build())
+        }
         AppCache.init(this, "todoWanAndroid")
     }
 
     override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
-        Log.d(com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LOG_TAG, " ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LEFT_ARROW}${activity?.javaClass?.simpleName.orEmpty()} is onCreated ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.RIGHT_ARROW}")
+        Log.d(
+            LOG_TAG,
+            " $LEFT_ARROW${activity?.javaClass?.simpleName.orEmpty()} is onCreated $RIGHT_ARROW")
     }
 
     override fun onActivityStarted(activity: Activity?) {
-        Log.d(com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LOG_TAG, " ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LEFT_ARROW}${activity?.javaClass?.simpleName.orEmpty()} is onStarted ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.RIGHT_ARROW}")
+        Log.d(
+            LOG_TAG,
+            " $LEFT_ARROW${activity?.javaClass?.simpleName.orEmpty()} is onStarted $RIGHT_ARROW")
     }
 
     override fun onActivityResumed(activity: Activity?) {
-        Log.d(com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LOG_TAG, " ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LEFT_ARROW}${activity?.javaClass?.simpleName.orEmpty()} is onResumed ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.RIGHT_ARROW}")
+        Log.d(
+            LOG_TAG,
+            " $LEFT_ARROW${activity?.javaClass?.simpleName.orEmpty()} is onResumed $RIGHT_ARROW")
     }
 
     override fun onActivityPaused(activity: Activity?) {
-        Log.d(com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LOG_TAG, " ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LEFT_ARROW}${activity?.javaClass?.simpleName.orEmpty()} is onPaused ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.RIGHT_ARROW}")
+        Log.d(
+            LOG_TAG,
+            " $LEFT_ARROW${activity?.javaClass?.simpleName.orEmpty()} is onPaused $RIGHT_ARROW")
     }
 
     override fun onActivityStopped(activity: Activity?) {
-        Log.d(com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LOG_TAG, " ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LEFT_ARROW}${activity?.javaClass?.simpleName.orEmpty()} is onStopped ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.RIGHT_ARROW}")
+        Log.d(
+            LOG_TAG,
+            " $LEFT_ARROW${activity?.javaClass?.simpleName.orEmpty()} is onStopped $RIGHT_ARROW")
     }
 
     override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
-        Log.d(com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LOG_TAG, " ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LEFT_ARROW}${activity?.javaClass?.simpleName.orEmpty()} is onSaveInstanceState ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.RIGHT_ARROW}")
+        Log.d(
+            LOG_TAG,
+            " $LEFT_ARROW${activity?.javaClass?.simpleName.orEmpty()} is onSaveInstanceState $RIGHT_ARROW")
     }
 
     override fun onActivityDestroyed(activity: Activity?) {
-        Log.d(com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LOG_TAG, " ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.LEFT_ARROW}${activity?.javaClass?.simpleName.orEmpty()} is onDestroyed ${com.aqrlei.app.open.todowanandroid.base.BaseApplication.Companion.RIGHT_ARROW}")
+        Log.d(
+            LOG_TAG,
+            " $LEFT_ARROW${activity?.javaClass?.simpleName.orEmpty()} is onDestroyed $RIGHT_ARROW")
     }
 }
